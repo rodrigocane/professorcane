ce0bc12c92e28ad31da8ac9d4810faad6db5d301
matheusvigon <matheusvigon@gmail.com>
2025-04-01 21:50:38 -0300

    Alteração Título Matheus Vigon

--- php-balanco-ajax/balanco.php
+++ php-balanco-ajax/balanco.php
@@ -8,7 +8,7 @@ include "conexao.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registro de Inventário do Matheus</title>
+    <title>Registro de Inventário do Matheus Vigon</title>
     
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <style>
